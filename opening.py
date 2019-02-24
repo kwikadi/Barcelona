42b56fb23f162070110fb8ca6e2a9a689627caac
kwikadi <kwikadi@gmail.com>
2019-02-24 22:36:05 +0530

    slice and dice end to end for one table

--- opening.py
+++ opening.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import db
 
@@ -20,15 +20,29 @@ def compare_data():
 def slicendice(category):
     fields = []
     dropdown_data = []
+    fields = ["year","district_name","neighborhood_name","age"]
     if category == "deaths":
-        fields = ["year","district_name","neighborhood_name","age"]
+        if request.args:
+            query_cons = "select " + ", ".join(fields) + " from deaths_v where "
+            print(request.args.getlist('year'))
+            for i in request.args:
+                l = request.args.getlist(i)
+                # print(v)
+                # print(request.args.get(i))
+                bracket_value = (', '.join("'" + item + "'" for item in l))
+                print(bracket_value)
+                query_cons += i + " in (" + bracket_value + ") and "
+            query_cons += "1=1"
+            print(query_cons)
+            sample = db.query(connection, query_cons)            
+        else:
+            sample = db.query(connection, "select " + ", ".join(fields) + " from deaths_v limit 10")
         for field in fields:
             inter_data = db.query(connection, "select distinct(" + field + ") from deaths_v order by " + field )
             mapped_data = list(map(lambda x: x[0], inter_data))
             dropdown_data.append({field: mapped_data})
             # TODO: Replace with * ?
-        sample = db.query(connection, "select " + ", ".join(fields) + " from deaths_v limit 10")
-    return render_template("slice.html", data=dropdown_data, sample=sample )
+    return render_template("slice.html", data=dropdown_data, sample=sample, page=category )
 
 
 @app.route('/agg')
--- static/js/slice.js
+++ static/js/slice.js
@@ -25,3 +25,24 @@ $(document).ready(function() {
         width: '15%'
     });
 });
+
+function deaths() {
+    yearval = $("#year").select2("val");
+    distval = $("#district_name").select2("val");
+    neigval = $("#neighborhood_name").select2("val");
+    ageval = $("#age").select2("val");
+    finallocation = "/slice/deaths?" 
+    for (x in yearval) {
+        finallocation += "&year=" + yearval[x];
+    }
+    for (x in distval) {
+        finallocation += "&district_name=" + distval[x];
+    }
+    for (x in neigval) {
+        finallocation += "&neighborhood_name=" + neigval[x];
+    }
+    for (x in ageval) {
+        finallocation += "&age=" + ageval[x];
+    }
+    window.location.replace(finallocation);
+}
